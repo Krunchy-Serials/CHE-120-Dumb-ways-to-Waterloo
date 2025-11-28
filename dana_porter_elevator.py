11c8b2acbb5199e68fb2e15e08ffcc9293f45a25
Katie <156144888+Katieku2@users.noreply.github.com>
2025-11-28 13:16:23 -0500

    Update dana_porter_elevator.py

--- dana_porter_elevator.py
+++ dana_porter_elevator.py
@@ -6,7 +6,7 @@ Created on Thu Nov 27 16:58:03 2025
 """
 import random 
 
-wrong_choices = ['scream as loud as you can', 'climb out of the emergency trap door', 'study for your linear algebra exam', 'cry', 'call your mom','summon a goose to help you','jump', 'pry open the door','use mass balances', 'make a titration out of your tears', 'wipe your tears with your 102 midterm', 'meditate', 'write a lab report on your experimental findings']
+wrong_choices = ['scream as loud as you can', 'climb out of the emergency trap door', 'cry', 'call your mom','summon a goose to help you','jump', 'pry open the door','use mass balances', 'make a titration out of your tears', 'wipe your tears with your 102 midterm', 'meditate', 'write a lab report on your experimental findings']
 
 right_choices = ['study for your linear algebra exam! Theres no time to lose!', 'press the emergency button', 'Apply Le Chatelier’s Principle to reduce stress', 'wait for help to arrive', 'hack into the elevator mainframe (launch jupyter notebook)']
 
@@ -45,4 +45,5 @@ def escape_dana_porter_elevator():
     else:
         print('FAIL! You are forever stuck in the elevator')
     
+
 escape_dana_porter_elevator()
